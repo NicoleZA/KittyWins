3ee42a4bb91938869b664529b974ceebb20e1692
NicoleZA <nicole.orpen@gmail.com>
2016-04-04 20:28:34 +0200

    release

---
+++ release.bat
@@ -0,0 +1,2 @@
+@echo off
+tns build android --release --keyStorePath D:\GitHub\KittyWins\publish\kittywins.jks --keyStorePassword qwerty01 --keyStoreAlias kittywins --keyStoreAliasPassword qwerty01
